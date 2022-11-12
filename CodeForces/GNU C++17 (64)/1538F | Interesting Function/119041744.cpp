32a372e093d5657a19565e7dd502c1217cf91e73
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:28:50 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1538F | Interesting Function/119041744.cpp
@@ -0,0 +1,34 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int64_t l, r; cin >> l >> r;
+            int64_t curr = 1, ans = 0;
+            while (curr <= r) {
+                  ans += r / curr - l / curr;
+                  // debug(ans, curr);
+                  curr *= 10;
+            }
+            cout << ans << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
