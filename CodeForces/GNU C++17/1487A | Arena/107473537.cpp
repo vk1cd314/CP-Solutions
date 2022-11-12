a2640fc902e04f31511697716c4e3f5415ef01d9
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:35:25 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1487A | Arena/107473537.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#ifdef SENDHELP
+#include "debug.h"
+#else
+#define debug(args...)
+#endif
+ 
+int main() {
+      #ifdef SENDHELP
+            auto begin = std::chrono::high_resolution_clock::now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            vector <int> a(n); for (int& x : a) cin >> x;
+ 
+            sort(a.begin(), a.end());
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                  if (a[0] < a[i]) ++ans;
+            }
+ 
+            cout << ans << '\n';
+      }
+ 
+      #ifdef SENDHELP
+            auto end = std::chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << std::chrono::duration_cast <std::chrono::duration <double>>(end - begin).count() << " seconds]\n";  
+      #endif
+}
