1619f4671158ef571b8b452b82b4ad03c1448ed8
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:24:56 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/281A | Word Capitalization/129673242.cpp
@@ -0,0 +1,17 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      string s; cin >> s;
+      s[0] = toupper(s[0]);
+      cout << s << '\n';
+}
