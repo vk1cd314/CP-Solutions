2a18da1cd3bfd2528786492bd6c7d30f105a8845
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:23:30 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1589A | Mathematical Addition/135505171.cpp
@@ -0,0 +1,19 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int64_t u, v; cin >> u >> v;
+            cout << u * u << ' ' << -v * v << '\n';            
+      }
+}
