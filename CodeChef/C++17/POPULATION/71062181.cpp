38d31fed16af7a4e3d8799f60b6bc0e6f1f0dcf1
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:09:35 +0600

    Committing files

---
+++ CodeChef/C++17/POPULATION/71062181.cpp
@@ -0,0 +1,19 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+
+using namespace std;
+
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+
+      int t; cin >> t;
+      while (t--) {
+            int x, y, z; cin >> x >> y >> z;
+            cout << x - y + z << '\n';
+      }
+}
