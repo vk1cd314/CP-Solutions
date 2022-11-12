c3fa9cc8d21e45f17b286a0d3989ae36c94a7e58
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:29:38 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1529A | Eshag Loves Big Arrays/117183279.cpp
@@ -0,0 +1,31 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            map <int, int> cnt;
+            vector <int> a(n); for (int &x : a) cin >> x, cnt[x]++;
+            int mn = *min_element(a.begin(), a.end());
+            cout << n - cnt[mn] << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
