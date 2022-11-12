3bc82e5e3a34448aa82e16e6dbef08047f7af9d8
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:04:48 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc211_a/24633684.cpp
@@ -0,0 +1,26 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+
+using namespace std;
+
+int main() {
+      #ifdef Icry
+            auto begin = chrono::high_resolution_clock::now();
+      #endif
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+
+      double a, b; cin >> a >> b;
+      cout << fixed << setprecision(10);
+      cout << (a - b) / 3.0 + b << '\n';
+
+      #ifdef Icry
+            auto end = chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono::duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
