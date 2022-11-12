5e20721d651ba55d3e1a7afa0643db8dc2bb9c51
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:31:38 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1509A | Average Height/113278334.cpp
@@ -0,0 +1,36 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            vector <int> a[2];
+            for (int i = 0, x; i < n; ++i) {
+                  cin >> x;
+                  a[x % 2].push_back(x);
+            }
+ 
+            for (int x : a[0]) cout << x << ' ';
+            for (int x : a[1]) cout << x << ' ';
+            cout << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
