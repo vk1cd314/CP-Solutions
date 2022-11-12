7f594ee5e69be01851b2a8811461fc85d0c19ff0
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:26:52 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1551A | Polycarp and Coins/123549899.cpp
@@ -0,0 +1,30 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono::high_resolution_clock::now();
+      #endif
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            int ext = (n - 2 * (n / 3)) - n / 3;
+            if (ext < 2) cout << n - 2 * (n / 3) << ' ' << n / 3 << '\n';
+            else cout << n - 2 * (n / 3) - 2 << ' ' << n / 3 + 1 << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono::duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
