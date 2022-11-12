d3d0fc5fc0e9b5a59b69326b1e40e0325d18a8b9
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:27:17 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1530A | Binary Decimal/122778365.cpp
@@ -0,0 +1,32 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono::high_resolution_clock::now();
+      #endif
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int ans = INT_MIN;
+            string s; cin >> s;
+            for (char c : s) ans = max(ans, int(c - '0'));
+ 
+            if (ans == 0) ans++;
+            cout << ans << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono::duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
