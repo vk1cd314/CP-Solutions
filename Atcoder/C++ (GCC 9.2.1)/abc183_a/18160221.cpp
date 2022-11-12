e0ee9dd94fbf7c34e37af9a4f091b050d7dc7002
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:06:13 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc183_a/18160221.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef BOI
+#include "debug.h"
+#else
+#define debug(args...)
+#endif
+
+int main() {
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+
+      int x; cin >> x;
+      cout << (x < 0 ? 0 : x);      
+}
