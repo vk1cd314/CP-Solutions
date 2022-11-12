c26ee7cd970957fecb0dd1f15558c1d7b7a2b6c9
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:33:48 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1174A | Ehab Fails to Be Thanos/110125181.cpp
@@ -0,0 +1,28 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = std::chrono::high_resolution_clock::now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int n; cin >> n;
+      vector <int> a(2 * n); for (int& x : a) cin >> x;
+      sort(a.begin(), a.end());
+      if (a[0] == a.back()) cout << "-1\n";
+      else for (int x : a) cout << x << ' ';
+ 
+      #ifdef Icry
+            auto end = std::chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << std::chrono::duration_cast <std::chrono::duration <double>>(end - begin).count() << " seconds]\n";  
+      #endif
+}
