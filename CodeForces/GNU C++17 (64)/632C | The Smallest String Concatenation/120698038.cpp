9481ef6fb00f31e683f62f7543aa55dc57dc76fa
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:28:11 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/632C | The Smallest String Concatenation/120698038.cpp
@@ -0,0 +1,30 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono::high_resolution_clock::now();
+      #endif
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int n; cin >> n;
+      vector <string> a(n); for (auto &s : a) cin >> s;
+      sort(a.begin(), a.end(), [&](string a, string b) {
+            return a + b < b + a;
+      });
+ 
+      for (auto s : a) cout << s;
+ 
+      #ifdef Icry
+            auto end = chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono::duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
