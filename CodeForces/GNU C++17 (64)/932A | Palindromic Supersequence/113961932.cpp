5a1c14f6592143171f86a8475a279acd734a2a28
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:31:11 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/932A | Palindromic Supersequence/113961932.cpp
@@ -0,0 +1,27 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      string a; cin >> a;
+      cout << a;
+      reverse(a.begin(), a.end());
+      cout << a;
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
