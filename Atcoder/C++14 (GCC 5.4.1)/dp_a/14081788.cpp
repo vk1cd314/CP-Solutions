6203ae1b9822848436fd518a743a1137f1a860d2
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:07:53 +0600

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/dp_a/14081788.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+using namespace std;
+
+int n;
+
+void solve(){   
+    cin >> n;
+    vector<int> heights(n);
+    vector<int> dp(n, INT_MAX); 
+    for(int& it : heights)
+        cin >> it;
+    dp[0] = 0;
+    for(int i = 0; i < n; ++i){
+        for(int j = i + 1; j < i + 3 && j < n; ++j){
+            dp[j] = min(dp[j], dp[i] + abs(heights[i] - heights[j]));
+        }
+    }
+    cout << dp[n - 1] << endl;
+}
+ 
+signed main() {
+	fastio;
+	// int t; cin >> t;
+ 
+	// while(t--)
+		solve();
+ 
+	return 0;
+}
