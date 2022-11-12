bbd86e3d7a4e1bce9e0ad32a97f282f885379336
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:30:46 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1519A | Red and Blue Beans/114555086.cpp
@@ -0,0 +1,31 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int64_t r, b, d; cin >> r >> b >> d;
+            if (r > b) swap(r, b);
+            int64_t diff = b - r;
+            if (diff <= r * d) cout << "YES\n";
+            else cout << "NO\n";
+      }
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
