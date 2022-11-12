f2f75662a98bd58ca29a38aa322b9e769eeef19a
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:07:19 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc172_b/14831736.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define cpresent(c,x) ((c).find(x) != (c).end()) 
+#define vpresent(c,x) (find(all(c),x) != (c).end())
+#define endl '\n'
+#ifdef LOCAL
+#include "/home/iggaboi/Desktop/CP/debug.h"
+#else
+#define debug(Args...)
+#endif
+using ll = long long;
+using namespace __gnu_pbds;
+using namespace std;
+template<class T>
+using indexed_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+int a;
+
+void shit() {
+	cin >> a;
+    cout << a + a*a + a*a*a <<endl;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false),cin.tie(nullptr);
+	// int t; cin >> t;
+	// while(t--)
+		shit();
+	return 0;
+}
