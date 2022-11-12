cddcde11961d32586b46e09e9bbc0e1938eddc4e
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:26:37 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1552A | Subsequence Permutation/123889355.cpp
@@ -0,0 +1,33 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono::high_resolution_clock::now();
+      #endif
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            string s, ss; cin >> s;
+            ss = s;
+            sort(ss.begin(), ss.end());
+            int ans = 0;
+            for (int i = 0; i < n; ++i) ans += ss[i] != s[i];
+            cout << ans << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono::duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
