95396743bac72582aed48e0241edb48e649ec98e
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:20:33 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1660A | Vasya and Coins/151614385.cpp
@@ -0,0 +1,19 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int a, b; cin >> a >> b;
+            cout << (a ? a + 2 * b + 1 : 1) << '\n';
+      }
+}
