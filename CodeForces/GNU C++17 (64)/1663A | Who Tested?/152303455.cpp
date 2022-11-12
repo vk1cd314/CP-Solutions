014071dd01c62103d4f00898633d332e0f2a24e7
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:20:19 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1663A | Who Tested?/152303455.cpp
@@ -0,0 +1,16 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+ 
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+ 
+      cout << "BucketPotato\n";
+}
