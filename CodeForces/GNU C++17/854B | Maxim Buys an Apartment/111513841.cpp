8b0ec898c0d1905848b06fbf2a50b9be56dd707e
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:32:57 +0600

    Committing files

---
+++ CodeForces/GNU C++17/854B | Maxim Buys an Apartment/111513841.cpp
@@ -0,0 +1,28 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = std::chrono::high_resolution_clock::now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int64_t n, k; cin >> n >> k;
+      if (n == k || k == 0) {
+            cout << "0 0\n";
+            return 0;
+      }
+      cout << "1 " << min(k + k, n - k) << '\n';
+      #ifdef Icry
+            auto end = std::chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << std::chrono::duration_cast <std::chrono::duration <double>>(end - begin).count() << " seconds]\n";  
+      #endif
+}
