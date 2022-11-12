a76756b46e1d37437dec1daf9f18357638b0d384
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:28:58 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1538A | Stone Game/118975760.cpp
@@ -0,0 +1,34 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = chrono :: high_resolution_clock :: now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int t; cin >> t;
+      while (t--) {
+            int n; cin >> n;
+            vector <int> a(n); for (int &x : a) cin >> x;
+            int mx = max_element(a.begin(), a.end()) - a.begin();
+            int mn = min_element(a.begin(), a.end())- a.begin();
+            mx++, mn++;
+ 
+            int ans = min({max(mx, mn), mx + abs(n - mn) + 1, mn + abs(n - mx) + 1, max(n - mx + 1, n - mn + 1)});
+            cout << ans << '\n';
+      }
+ 
+      #ifdef Icry
+            auto end = chrono :: high_resolution_clock :: now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << chrono :: duration_cast <chrono :: duration <double>> (end - begin).count() << " seconds]\n";  
+      #endif
+}
