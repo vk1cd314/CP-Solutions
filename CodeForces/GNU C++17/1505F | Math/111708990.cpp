8a99c877da08c9d9891f3730aceb8bab3082fb0f
Bholanath Das Niloy <61154459+vk1cd314@users.noreply.github.com>
2022-11-13 00:32:49 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1505F | Math/111708990.cpp
@@ -0,0 +1,25 @@
+#ifdef Icry
+#include "debug.h"
+#else
+#include <bits/stdc++.h>
+#define debug(args...) 
+#endif
+ 
+using namespace std;
+ 
+int main() {
+      #ifdef Icry
+            auto begin = std::chrono::high_resolution_clock::now();
+      #endif
+      ios_base :: sync_with_stdio(0);
+      cin.tie(0);
+ 
+      int a; cin >> a;
+      cout << 2 - a * a << '\n';
+ 
+      #ifdef Icry
+            auto end = std::chrono::high_resolution_clock::now();
+            cerr << setprecision(4) << fixed;
+            cerr << "[Execution time: " << std::chrono::duration_cast <std::chrono::duration <double>>(end - begin).count() << " seconds]\n";  
+      #endif
+}
